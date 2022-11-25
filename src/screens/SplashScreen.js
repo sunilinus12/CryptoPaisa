20b55169eb5b6f72d9a3df5faea8b6de822c3568
sunilinus12 <www.suniljaunwal2@gmail.com>
2022-11-25 13:30:08 +0530

    added the splash screen.. and folder structure..

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,44 @@
+import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function SplashScreen() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.inner_container}>
+        <Image
+          source={require('../assets/splash.png')}
+          style={{
+            width: '40%',
+            height: '25%',
+          }}
+          resizeMode="contain"
+        />
+        <Text style={styles.splash_text}>Crypto Paisa</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: '5%',
+    paddingBottom: '13%',
+  },
+  inner_container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  splash_text: {
+    color: 'black',
+    textTransform: 'uppercase',
+    textAlign: 'center',
+    marginVertical: 10,
+    fontSize: 20,
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: -1, height: 9},
+    textShadowRadius: 10,
+  },
+});
